6c065099ca46b16b6db458cb6b04677677924c3a
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-05 15:33:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,17 @@
 provider "aws" {
   region = "eu-north-1"
+  access_key  = var.aws_access_key
+  secret_key  = var.aws_secret_key
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
 }
 
 resource "aws_vpc" "my_vpc" {
