8931c27bde35ca3e8bab3f2b986d72811b4a78ee
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-08 03:37:03 +0530

    Update backend.sh
    container cleanup

--- backend.sh
+++ backend.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+if [ "$(sudo docker ps -q -f name=backend)" ]; then
+    sudo docker stop backend
+    sudo docker rm backend
+fi
 
 sudo apt-get update
-
-
 sudo apt-get install -y docker.io
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
 sudo docker pull jai108/dev_project-backend:latest
+
 sudo docker run -d -p 5000:5000 --name backend jai108/dev_project-backend:latest
