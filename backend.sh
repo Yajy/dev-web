a64a3fe646794ab320f5db7bfbbdc26adb2ed5fb
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-08 00:07:12 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -6,12 +6,8 @@ sudo apt-get update
 
 sudo apt-get install -y docker.io
 
-# Start and enable Docker service
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Pull the Docker image
 sudo docker pull jai108/dev_project-backend:latest
-
-# Run the Docker container
 sudo docker run -d -p 5000:5000 --name backend jai108/dev_project-backend:latest
