2ebcb038781285df93ef51979367d2f3154186a3
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-08 03:36:21 +0530

    Update frontend.sh
    existing container cleanup

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+if [ "$(sudo docker ps -q -f name=frontend)" ]; then
+    sudo docker stop frontend
+    sudo docker rm frontend
+fi
+
 sudo apt-get update
 sudo apt-get install -y docker.io
-
 
 sudo systemctl start docker
 sudo systemctl enable docker
 
-
 sudo docker pull nginx:latest
 
-
-sudo docker run -d -p 80:80 --name frontend_new nginx:latest
-
+sudo docker run -d -p 80:80 --name frontend nginx:latest
