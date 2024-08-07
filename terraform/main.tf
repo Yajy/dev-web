1fd736034d861d38a54425d69b5e2c2ac2532305
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-07 22:57:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -108,6 +108,13 @@ resource "aws_security_group" "frontend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  engress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -141,6 +148,13 @@ resource "aws_security_group" "backend_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  engress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
