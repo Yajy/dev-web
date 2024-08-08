915c6aecaaa1a9f7d2bb91f6d2054fc63ada8308
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-08 21:32:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -326,3 +326,9 @@ output "frontend_public_ip" {
 output "backend_private_ip" {
   value = aws_instance.backend.private_ip
 }
+
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+  description = "The public IP address of the bastion host."
+}
+
