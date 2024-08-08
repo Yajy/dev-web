5c1bde65c6ec8f2ab5ade60d903eeff0216ebd92
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-08 21:03:02 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Stop and remove existing backend and mysql containers
 if [ "$(sudo docker ps -q -f name=backend)" ]; then
     sudo docker stop backend
     sudo docker rm backend
@@ -10,16 +11,22 @@ if [ "$(sudo docker ps -q -f name=mysql)" ]; then
     sudo docker rm mysql
 fi
 
+# Install Docker if not already installed
 sudo apt-get update
 sudo apt-get install -y docker.io
 
+# Start Docker service
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo docker pull mysql:5.7
+# Create a Docker network
+sudo docker network create my_network
 
+# Run MySQL container
+sudo docker pull mysql:5.7
 sudo docker run -d \
     --name mysql \
+    --network my_network \
     -e MYSQL_ROOT_PASSWORD=password \
     -e MYSQL_DATABASE=testdb \
     -p 3306:3306 \
@@ -28,13 +35,13 @@ sudo docker run -d \
 echo "Waiting for MySQL to start..."
 sleep 40
 
+# Run Backend container
 sudo docker pull jai108/dev_project-backend:latest
-
 sudo docker run -d \
     -p 5000:5000 \
     --name backend \
-    --network="host" \
-    -e MYSQL_HOST=localhost \
+    --network my_network \
+    -e MYSQL_HOST=mysql \
     -e MYSQL_USER=root \
     -e MYSQL_PASSWORD=password \
     -e MYSQL_DATABASE=testdb \
