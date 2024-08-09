fcc8cd41320e127d39580f40d2efb6840955c407
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-08-10 04:05:23 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -11,6 +11,6 @@ sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo docker pull nginx:latest
+sudo docker pull form-front:latest
 
-sudo docker run -d -p 80:80 --name frontend nginx:latest
+sudo docker run -d -p 80:80 --name frontend form-front:latest
