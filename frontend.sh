38e2c7853a11c941a05cf5ea09f8fdc1ca037e32
Yajy Goswami <101127424+Yajy@users.noreply.github.com>
2024-09-09 20:02:33 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$(sudo docker ps -q -f name=frontend)" ]; then
-    sudo docker stop frontend
-    sudo docker rm frontend
+if [ "$(sudo docker ps -q -f name=my-apache-app)" ]; then
+    sudo docker stop my-apache-app
+    sudo docker rm my-apache-app
 fi
 
 sudo apt-get update
@@ -11,6 +11,5 @@ sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo docker pull jai108/form-front:latest
-
-sudo docker run -d -p 80:80 --name frontend jai108/form-front:latest
+sudo docker pull httpd
+sudo docker run -dit --name my-apache-app -p 9090:80 -v "/home/ubuntu":/usr/local/apache2/htdocs/ httpd:2.4
